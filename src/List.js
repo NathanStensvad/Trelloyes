750d5f55225fb99edfae78eca4d0a44e93e26e51
Nathan Stensvad <nstensvad@hotmail.com>
2020-09-09 01:16:18 -0700

    Callback props

--- src/List.js
+++ src/List.js
@@ -5,16 +5,27 @@ import './List.css';
 
 function List(props) {
     return (
-    <div className='List'>
-        <header className='List-header'>
-            <h2>{props.header}</h2>
-        </header>
-        <div className='List-cards'>
-            {props.cards.map(card =>
-            <Card title = {card.title} content = {card.content}/>
-            )}
+        <div className='List'>
+            <header className='List-header'>
+                <h2>{props.header}</h2>
+                <button
+                onClick = {() => props.onRandomCard(props.id)}
+                type="button"
+                >Add Random Card</button>
+            </header>
+            <div className='List-cards'>
+                {props.cards.map(card =>
+                    <Card
+                        key={card.id}
+                        id={card.id}
+                        title={card.title}
+                        content={card.content}
+                        onDeleteCard={props.onDeleteCard}
+                        onRandomCard={props.onRandomCard}
+                    />
+                )}
+            </div>
         </div>
-    </div>
     )
 }
 
